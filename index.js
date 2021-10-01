f462ff4399c015f1847ea6ea53a96bb2ea7d2d9f
Ashish Kumar Mandal <666kmandal@gmail.com>
2021-10-01 12:42:57 +0530

    crud operation done

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const getData = async (collectionName)=>{
 }
 
 const setData = async (collectionName,newRecords)=>{
-    let db = await dbc(collectionName,newRecords);
-    let result = await db.insertOne(newRecords);
+    let data = await dbc(collectionName);
+    let result = await data.insertOne(newRecords);
     
     if(result.acknowledged){
         console.log("New Record Saved Successfully");
@@ -17,12 +17,32 @@ const setData = async (collectionName,newRecords)=>{
     }
 }
 
-const records = {
-    "name":"Shiva",
-    "brand":"Paypines",
-    "price":123654
+const putData = async (collectionName,condition,updateRecords)=>{
+    let data = await dbc(collectionName);
+    let result = await data.updateMany(condition,{$set:updateRecords});
+    
+    if(result.acknowledged){
+        console.log("Record Updated Successfully");
+    }else{
+        console.log("process Failed");
+    }
 }
 
-setData('products',records);
+const deleteData = async (collectionName,condition)=>{
+    let data = await dbc(collectionName);
+    let result = await data.deleteMany(condition);
+    
+    if(result.acknowledged){
+        console.log("Record Deleted Successfully");
+    }else{
+        console.log("process Failed");
+    }
+}
+
+// setData('products',{"name":"Vivo pro 9", "brand":"Vivo", "price":10999});
+
+// putData('products',{"name":"Shiva"},{"name":"Vivo Hexa", "brand":"Vivo", "price":9999});
+
+// deleteData('products',{"name":"Vivo Hexa"});
 
 getData('products');
